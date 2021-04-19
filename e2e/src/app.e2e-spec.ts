7ab9e3dd0b4a23b524a969d8fbe51b5f6c356d9e
Sebastian Soto Madrigal <s.m.sebastian.n@gmail.com>
2021-04-19 17:39:24 -0600

    initial commit

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', async () => {
     await page.navigateTo();
-    expect(await page.getTitleText()).toEqual('clean-template app is running!');
+    expect(await page.getTitleText()).toEqual('grafo-dijkstra app is running!');
   });
 
   afterEach(async () => {
